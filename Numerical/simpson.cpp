0e9e6204f7ecca7e34ec204225c9e8b230268fd4
MD Nasir Uddin Rubel <96114807+nurubel1819@users.noreply.github.com>
2023-09-27 00:09:42 +0600

    Update simpson.cpp

--- Numerical/simpson.cpp
+++ Numerical/simpson.cpp
@@ -31,3 +31,38 @@ int main()
     cout<< "Ans is : "<<ans<<endl;
     return 0;
 }
+/*
+#include<bits/stdc++.h>
+using namespace std;
+
+double funciton_simpsons(double x)
+{
+    return 1/x;
+}
+
+double del_x_function(double upper_limit,double lower_limit,int n)
+{
+    return (upper_limit-lower_limit)/n;
+}
+
+int main()
+{
+    cout<< "Simpshon rule "<<endl;
+    double l,u,n;
+    l=1;///lower limit
+    u=2;///upper limit
+    n=10;
+    double del_x = del_x_function(u,l,n);
+    double xi,ans=0;
+    for(int i=0;i<=n;i++)
+    {
+        xi = l+(i*del_x);
+        if(i==0 or i==n) ans+=funciton_simpsons(xi);
+        else if(i%3==0) ans+=funciton_simpsons(xi)*2;
+        else ans+=funciton_simpsons(xi)*3;
+    }
+    ans = ans * ((del_x*3)/8);
+    cout<< "Ans is : "<<ans<<endl;
+    return 0;
+}
+*/
